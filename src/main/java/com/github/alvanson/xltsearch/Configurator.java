0d3777f104e9e07ab368bdaa0039ff6feddeb9b4
Evan Thompson <alvanson@users.noreply.github.com>
2016-08-01 16:20:41 -0500

    Port Configurator to FXML

--- src/main/java/com/github/alvanson/xltsearch/Configurator.java
+++ src/main/java/com/github/alvanson/xltsearch/Configurator.java
@@ -23,6 +23,8 @@ import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.collections.FXCollections;
 import javafx.concurrent.Task;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
@@ -36,9 +38,7 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 class Configurator {
@@ -46,147 +46,57 @@ class Configurator {
     private static final int SCENE_HEIGHT = 480;
     private static final int BUTTON_WIDTH = 120;
 
+    @FXML private BorderPane border;
+    @FXML private TextField newField;
+    @FXML private ListView<String> listView;
+
     private Config config;
     private boolean dirty;
-
     private final Stage stage = new Stage();
-    private final ListView<String> listView = new ListView<>();
     private final Map<String,ComboBox<String>> optionsMap = new HashMap<>();
-
     private final ObjectProperty<Catalog> catalog = new SimpleObjectProperty<>();
 
     Configurator() {
         stage.setTitle("Configuration");
-        catalog.addListener((o, oldValue, newValue) -> {
-            if (newValue != null) {
-                // draw UI for new catalog
-                dirty = false;
-                initUI();
-            } else {
-                // clear UI
-                stage.setScene(new Scene(new BorderPane()));
-            }
-        });
+
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/configurator.fxml"));
+        fxmlLoader.setController(this);
+        try {
+            Scene scene = new Scene(fxmlLoader.load(), SCENE_WIDTH, SCENE_HEIGHT);
+            scene.getStylesheets().add("stylesheet.css");
+            stage.setScene(scene);
+        } catch (Exception ex) {
+            DetailedAlert alert = new DetailedAlert(DetailedAlert.AlertType.ERROR);
+            alert.setTitle("Error");
+            alert.setHeaderText("Exception while loading Configurator");
+            alert.setDetailsText(Message.getStackTrace(ex));
+            alert.showAndWait();
+        }
     }
 
-    private void initUI() {
-        final BorderPane border = new BorderPane();
+    @FXML
+    private void initialize() {
+        catalog.addListener((o, oldValue, newValue) -> {
+            dirty = false;
+            if (newValue != null) {
+                List<String> configs = newValue.getConfigs();
+                Collections.sort(configs);
+                listView.setItems(FXCollections.observableList(configs));
+            } else {
+                listView.getItems().clear();
+            }
+            drawCenterPane();
+        });
 
-        // LEFT
-        final VBox leftVBox = new VBox(10);
-        leftVBox.setPadding(new Insets(10));
-
-        final HBox newHBox = new HBox(10);
-        final TextField newField = new TextField();
-        final Button newButton = new Button("New");
-
-        newHBox.getChildren().addAll(newField, newButton);
-        newHBox.setHgrow(newField, Priority.ALWAYS);
-
-        List<String> configs = catalog.get().getConfigs();
-        Collections.sort(configs);
-        listView.setItems(FXCollections.observableList(configs));
-
-        leftVBox.getChildren().addAll(newHBox, listView);
-        leftVBox.setVgrow(listView, Priority.ALWAYS);
-
-        border.setLeft(leftVBox);
-
-        // BOTTOM
-        final HBox bottomHBox = new HBox(10);
-        bottomHBox.setPadding(new Insets(10));
-
-        final Button aboutButton = new Button("About");
-        aboutButton.setPrefWidth(BUTTON_WIDTH);
-        final Label spacer = new Label();   // ugly hack
-        spacer.setMaxWidth(Double.MAX_VALUE);
-        final Button loadButton = new Button("Load");
-        loadButton.setPrefWidth(BUTTON_WIDTH);
-        final Button cancelButton = new Button("Cancel");
-        cancelButton.setPrefWidth(BUTTON_WIDTH);
-
-        bottomHBox.getChildren().addAll(aboutButton, spacer, loadButton, cancelButton);
-        bottomHBox.setHgrow(spacer, Priority.ALWAYS);
-        bottomHBox.setMinHeight(HBox.USE_PREF_SIZE);
-
-        border.setBottom(bottomHBox);
-
-        // CENTER
-        drawCenterPane(border);
-
-        // UI BINDINGS
         listView.getSelectionModel().selectedItemProperty().addListener(
                 (o, oldValue, newValue) -> {
             dirty = false;
             config = catalog.get().getConfig(newValue);
-            drawCenterPane(border);
+            drawCenterPane();
         });
-
-        // CALLBACKS
-        newButton.setOnAction((event) -> {
-            // validate name (only allow word characters)
-            String name = newField.getText();
-            name = name.replaceAll("[^\\w]","");
-            newField.clear();
-            newField.setPromptText("");
-            if (!name.equals("")) {
-                int index = Collections.binarySearch(listView.getItems(), name);
-                if (index < 0) {    // not found in list (good)
-                    // add in sorted order and select
-                    listView.getItems().add(-index-1, name);
-                    listView.getSelectionModel().select(-index-1);
-                } else {
-                    listView.getSelectionModel().select(index);
-                }
-            } else {
-                newField.setPromptText("Invalid name");
-            }
-        });
-
-        aboutButton.setOnAction((event) -> {
-            Alert alert = new Alert(Alert.AlertType.INFORMATION);
-            alert.setTitle("About");
-            alert.setHeaderText("XLTSearch " + Config.XLT_VERSION);
-            alert.setContentText("Copyright 2016 Evan A. Thompson\n" +
-                                 "https://github.com/alvanson/xltsearch");
-            alert.showAndWait();
-        });
-
-        loadButton.setOnAction((event) -> {
-            if (config == null) {
-                Alert alert = new Alert(Alert.AlertType.ERROR);
-                alert.setTitle("Error");
-                alert.setHeaderText("No configuration selected");
-                alert.setContentText("");
-                alert.showAndWait();
-                return;
-            } else if (dirty) {
-                Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
-                alert.setTitle("Confirmation");
-                alert.setHeaderText("Configuration not saved");
-                alert.setContentText("Do you want to save?");
-                alert.getDialogPane().getButtonTypes().clear();
-                alert.getDialogPane().getButtonTypes().addAll(
-                    ButtonType.YES, ButtonType.NO, ButtonType.CANCEL);
-                Optional<ButtonType> result = alert.showAndWait();
-                if (!result.isPresent() || result.get() == ButtonType.CANCEL) {
-                    return;
-                } else if (result.get() == ButtonType.YES) {
-                    saveConfig(border);
-                }
-            }
-            catalog.get().loadConfig(config.getName());
-            hide();
-        });
-
-        cancelButton.setOnAction((event) -> hide());
-
-        Scene scene = new Scene(border, SCENE_WIDTH, SCENE_HEIGHT);
-        scene.getStylesheets().add("stylesheet.css");
-        stage.setScene(scene);
     }
 
-    private void drawCenterPane(BorderPane border) {
+    private void drawCenterPane() {
         final GridPane grid = new GridPane();
         grid.setAlignment(Pos.BASELINE_LEFT);
         grid.setPadding(new Insets(10, 10, 10, 0));
@@ -199,7 +109,10 @@ class Configurator {
         grid.setHgap(10);
         grid.setVgap(10);
 
-        if (config != null) {
+        if (config == null) {
+           final Label label = new Label("No configuration selected");
+           grid.add(label, 0, 0);
+        } else {
             // add each property, one per row
             int row = 0;
             optionsMap.clear();
@@ -272,85 +185,107 @@ class Configurator {
 
             grid.add(deleteButton, 2, row+5);
 
-            // CALLBACKS
-            updateIndexButton.setOnAction((event) -> {
-                if (dirty) {
-                    Alert alert = new Alert(Alert.AlertType.ERROR);
-                    alert.setTitle("Error");
-                    alert.setHeaderText("Configuration not saved");
-                    alert.setContentText("Configuration must be saved before updating index.");
-                    alert.showAndWait();
-                } else {
-                    Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
-                    alert.setTitle("Confirmation");
-                    alert.setHeaderText("Update index");
-                    alert.setContentText("Are you sure you want to load this configuration\n" +
-                        "and update the index?");
-                    Optional<ButtonType> result = alert.showAndWait();
-                    if (result.isPresent() && result.get() == ButtonType.OK) {
-                        catalog.get().loadConfig(config.getName());
-                        catalog.get().updateIndex();
-                        hide();
-                    }
-                }
-            });
-
-            clearIndexButton.setOnAction((event) -> {
-                if (dirty) {
-                    Alert alert = new Alert(Alert.AlertType.ERROR);
-                    alert.setTitle("Error");
-                    alert.setHeaderText("Configuration not saved");
-                    alert.setContentText("Configuration must be saved before clearing index.");
-                    alert.showAndWait();
-                } else {
-                    Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
-                    alert.setTitle("Confirmation");
-                    alert.setHeaderText("Clear index");
-                    alert.setContentText("Are you sure you want to clear the index?");
-                    Optional<ButtonType> result = alert.showAndWait();
-                    if (result.isPresent() && result.get() == ButtonType.OK) {
-                        config.clearIndex();
-                        drawCenterPane(border);
-                    }
-                }
-            });
-
-            saveButton.setOnAction((event) -> saveConfig(border));
-
-            deleteButton.setOnAction((event) -> {
-                Config candidate = config;   // avoid race conditions
-                // confim delete
-                if (catalog.get().getConfigName().equals(candidate.getName())) {
-                    Alert alert = new Alert(Alert.AlertType.ERROR);
-                    alert.setTitle("Error");
-                    alert.setHeaderText("Current configuration");
-                    alert.setContentText("Cannot delete current configuration.");
-                    alert.showAndWait();
-                } else {
-                    Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
-                    alert.setTitle("Confirmation");
-                    alert.setHeaderText("Delete configuration");
-                    alert.setContentText("Are you sure you want to delete this configuration?\n" +
-                                         "This operation cannot be undone.");
-                    Optional<ButtonType> result = alert.showAndWait();
-                    if (result.isPresent() && result.get() == ButtonType.OK) {
-                        // config is not guaranteed to be == candidate or != null, so do not use
-                        listView.getItems().remove(candidate.getName());
-                        listView.getSelectionModel().clearSelection();
-                        candidate.delete();
-                        candidate = null;
-                    }
-                }
-            });
-        } else {
-            final Label label = new Label("No configuration selected");
-            grid.add(label, 0, 0);
+            updateIndexButton.setOnAction((event) -> updateIndex());
+            clearIndexButton.setOnAction((event) -> clearIndex());
+            saveButton.setOnAction((event) -> saveConfig());
+            deleteButton.setOnAction((event) -> deleteConfig());
         }
 
         border.setCenter(grid);
     }
 
-    private void saveConfig(BorderPane border) {
+    @FXML
+    private void newConfig() {
+        // validate name (only allow word characters)
+        String name = newField.getText();
+        name = name.replaceAll("[^\\w]","");
+        newField.clear();
+        newField.setPromptText("");
+        if (!name.equals("")) {
+            int index = Collections.binarySearch(listView.getItems(), name);
+            if (index < 0) {    // not found in list (good)
+                // add in sorted order and select
+                listView.getItems().add(-index-1, name);
+                listView.getSelectionModel().select(-index-1);
+            } else {
+                listView.getSelectionModel().select(index);
+            }
+        } else {
+            newField.setPromptText("Invalid name");
+        }
+    }
+
+    @FXML
+    private void loadConfig() {
+        if (config == null) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Error");
+            alert.setHeaderText("No configuration selected");
+            alert.setContentText("");
+            alert.showAndWait();
+            return;
+        } else if (dirty) {
+            Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+            alert.setTitle("Confirmation");
+            alert.setHeaderText("Configuration not saved");
+            alert.setContentText("Do you want to save?");
+            alert.getDialogPane().getButtonTypes().clear();
+            alert.getDialogPane().getButtonTypes().addAll(
+                ButtonType.YES, ButtonType.NO, ButtonType.CANCEL);
+            Optional<ButtonType> result = alert.showAndWait();
+            if (!result.isPresent() || result.get() == ButtonType.CANCEL) {
+                return;
+            } else if (result.get() == ButtonType.YES) {
+                saveConfig();
+            }
+        }
+        catalog.get().loadConfig(config.getName());
+        hide();
+    }
+
+    private void updateIndex() {
+        if (dirty) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Error");
+            alert.setHeaderText("Configuration not saved");
+            alert.setContentText("Configuration must be saved before updating index.");
+            alert.showAndWait();
+        } else {
+            Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+            alert.setTitle("Confirmation");
+            alert.setHeaderText("Update index");
+            alert.setContentText("Are you sure you want to load this configuration\n" +
+                "and update the index?");
+            Optional<ButtonType> result = alert.showAndWait();
+            if (result.isPresent() && result.get() == ButtonType.OK) {
+                catalog.get().loadConfig(config.getName());
+                catalog.get().updateIndex();
+                hide();
+            }
+        }
+    }
+
+    private void clearIndex() {
+        if (dirty) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Error");
+            alert.setHeaderText("Configuration not saved");
+            alert.setContentText("Configuration must be saved before clearing index.");
+            alert.showAndWait();
+        } else {
+            Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+            alert.setTitle("Confirmation");
+            alert.setHeaderText("Clear index");
+            alert.setContentText("Are you sure you want to clear the index?");
+            Optional<ButtonType> result = alert.showAndWait();
+            if (result.isPresent() && result.get() == ButtonType.OK) {
+                config.clearIndex();
+                drawCenterPane();
+            }
+        }
+    }
+
+    private void saveConfig() {
         // saving is potentially expensive: confirm with user before saving
         if (config == null) {
             Alert alert = new Alert(Alert.AlertType.ERROR);
@@ -377,7 +312,33 @@ class Configurator {
             }
         }
         dirty = false;
-        drawCenterPane(border);
+        drawCenterPane();
+    }
+
+    private void deleteConfig() {
+        Config candidate = config;   // avoid race conditions
+        // confim delete
+        if (catalog.get().getConfigName().equals(candidate.getName())) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Error");
+            alert.setHeaderText("Current configuration");
+            alert.setContentText("Cannot delete current configuration.");
+            alert.showAndWait();
+        } else {
+            Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+            alert.setTitle("Confirmation");
+            alert.setHeaderText("Delete configuration");
+            alert.setContentText("Are you sure you want to delete this configuration?\n" +
+                                 "This operation cannot be undone.");
+            Optional<ButtonType> result = alert.showAndWait();
+            if (result.isPresent() && result.get() == ButtonType.OK) {
+                // config is not guaranteed to be == candidate or != null, so do not use
+                listView.getItems().remove(candidate.getName());
+                listView.getSelectionModel().clearSelection();
+                candidate.delete();
+                candidate = null;
+            }
+        }
     }
 
     void show() {
@@ -404,6 +365,17 @@ class Configurator {
         stage.show();
     }
 
+    @FXML
+    private void showAbout() {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("About");
+        alert.setHeaderText("XLTSearch " + Config.XLT_VERSION);
+        alert.setContentText("Copyright 2016 Evan A. Thompson\n" +
+                             "https://github.com/alvanson/xltsearch");
+        alert.showAndWait();
+    }
+
+    @FXML
     void hide() {
         listView.getSelectionModel().clearSelection();
         stage.hide();
